d47505b9c73cd05e7098109af1257a95206cdfd5
Walaa Abu Lebdeh <wala.abulebdeh@gmail.com>
2021-04-12 14:16:00 +0300

    add dist folder

--- dist/utils.js
+++ dist/utils.js
@@ -1,10 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
 function invertNumber(min, max, num) {
   return max + min - num;
 }
 
 function convertRange(oldMin, oldMax, newMin, newMax, oldValue) {
-  const percent = (oldValue - oldMin) / (oldMax - oldMin);
-  const result = percent * (newMax - newMin) + newMin;
+  var percent = (oldValue - oldMin) / (oldMax - oldMin);
+  var result = percent * (newMax - newMin) + newMin;
   return result || 0;
 }
 
@@ -12,16 +19,20 @@ function convertWidthToPx(width, containerWidth) {
   if (typeof width === "number") {
     return width;
   }
+
   if (typeof width !== "string") {
-    throw new Error(`Received: ${width} - Size must be a number or string`);
+    throw new Error("Received: ".concat(width, " - Size must be a number or string"));
   }
+
   if (width.substr(-1) === "%") {
-    const percent = 100 / Number(width.slice(0, -1));
+    var percent = 100 / Number(width.slice(0, -1));
     return containerWidth / percent;
   }
+
   if (width.substr(-2) === "px") {
     return Number(width.slice(0, -2));
   }
+
   return Number(width);
 }
 
@@ -29,15 +40,17 @@ function convertWidthToString(width) {
   if (typeof width === "number") {
     return width + "px";
   }
+
   return width;
 }
 
 function noop() {}
 
-export default {
-  invertNumber,
-  convertRange,
-  convertWidthToPx,
-  convertWidthToString,
-  noop
+var _default = {
+  invertNumber: invertNumber,
+  convertRange: convertRange,
+  convertWidthToPx: convertWidthToPx,
+  convertWidthToString: convertWidthToString,
+  noop: noop
 };
+exports["default"] = _default;
